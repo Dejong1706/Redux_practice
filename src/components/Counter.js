eaaf6b81e19bccf32e7e7906fbfed65d00f23553
JanBe <ppk1998@naver.com>
2024-03-27 21:47:01 +0900

    redux-toolkit practice

---
+++ src/components/Counter.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/counter";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  console.log(counter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div className={classes.buttonli}>
+        <button onClick={incrementHandler}>증가</button>
+        <button onClick={increaseHandler}>5씩 증가</button>
+        <button onClick={decrementHandler}>감소</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
